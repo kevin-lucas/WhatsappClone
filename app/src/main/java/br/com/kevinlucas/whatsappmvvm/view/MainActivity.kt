7bdd9ea358eda447c66991252c3528ebbcf9f812
Kevin Lucas <55221269+kevin-lucas@users.noreply.github.com>
2021-06-02 14:52:45 -0300

    Adicionando a toolbar a tela principal

--- app/src/main/java/br/com/kevinlucas/whatsappmvvm/view/MainActivity.kt
+++ app/src/main/java/br/com/kevinlucas/whatsappmvvm/view/MainActivity.kt
@@ -3,16 +3,25 @@ package br.com.kevinlucas.whatsappmvvm.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import br.com.kevinlucas.whatsappmvvm.R
 import br.com.kevinlucas.whatsappmvvm.service.repository.remote.FirebaseClient
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar_main)
+        toolbar.setTitle("WhatsApp")
+
+        setSupportActionBar(toolbar)
 
         setListeners()
     }
@@ -33,6 +42,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             startActivity(it)
             finish()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
 }
