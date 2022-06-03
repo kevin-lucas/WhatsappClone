e5d8556ba71c358385a80a579643b8e5efc9ea8a
Kevin Lucas <55221269+kevin-lucas@users.noreply.github.com>
2022-06-03 19:37:44 -0300

    update gradle

--- app/src/main/java/br/com/kevinlucas/whatsappmvvm/service/model/ChatModel.kt
+++ app/src/main/java/br/com/kevinlucas/whatsappmvvm/service/model/ChatModel.kt
@@ -1,3 +1,7 @@
 package br.com.kevinlucas.whatsappmvvm.service.model
 
-class ChatModel(val idContact: String? = null, val name: String? = null, val message: String? = null)
+class ChatModel(
+    val idContact: String? = null,
+    val name: String? = null,
+    val message: String? = null
+)
